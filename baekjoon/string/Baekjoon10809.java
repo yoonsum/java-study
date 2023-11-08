84ddd73e3a0c69c54831adfbf6935241ed17a154
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-08 19:59:34 +0900

    백준 10809 - 알파벳 개수만큼의 int 배열을 생성하고 Arrays.fill 사용해서 배열의 모든값을 -1로 초기화 - 입력받은 단어를 char배열로 변환해서 저장 - 단어의 알파벳에 해당하는 int 배열의 위치 값이 -1이 아니면 단어의 알파벳위치값 저장 - int 배열 출력

--- baekjoon/string/Baekjoon10809.java
+++ baekjoon/string/Baekjoon10809.java
@@ -5,11 +5,25 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Baekjoon10809 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] S = br.readLine().toCharArray();
+        int[] alpabet = new int[26];
+        Arrays.fill(alpabet, -1);
+
+        for (int i = 0; i < S.length; i++) {
+            if (alpabet[S[i] - 'a'] == -1) {
+                alpabet[S[i] - 'a'] = i;
+            }
+        }
+        for (int a : alpabet) {
+            bw.write(Integer.toString(a) + " ");
+        }
 
         bw.flush();
         bw.close();
