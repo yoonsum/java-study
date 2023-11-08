aede3f1f104c1a1ebc64f09b34ba4474c7094a50
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-08 20:56:02 +0900

    백준 1152 - 대소문자와 공백으로 이루어진 문자열을 입력받아서 공백으로 문자열 쪼갬 - StringTokenizer의 countTokens 함수 사용해서 단어개수 출력

---
+++ baekjoon/string/Baekjoon1152.java
@@ -0,0 +1,21 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Baekjoon1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        bw.write(Integer.toString(st.countTokens()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
