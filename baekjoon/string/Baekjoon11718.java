287fa8537125927ef19bbaaf85b3de354daa9867
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-09 10:44:02 +0900

    백준 11718 - EOF 될때까지 입력받아서 출력

---
+++ baekjoon/string/Baekjoon11718.java
@@ -0,0 +1,23 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            bw.write(str + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
