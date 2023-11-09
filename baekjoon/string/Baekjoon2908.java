c4d4fd0561071d0d0098505d8bd971553b6e6199
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-09 10:16:51 +0900

    백준 2908 - 두 세자리수를 입력받음 - 두 수를 StringBuffer의 reverse를 사용해서 뒤집음 - 두 수를 비교해서 더 큰수를 출력

---
+++ baekjoon/string/Baekjoon2908.java
@@ -0,0 +1,31 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.lang.StringBuffer;
+
+public class Baekjoon2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(new StringBuffer(st.nextToken()).reverse().toString());
+        int num2 = Integer.parseInt(new StringBuffer(st.nextToken()).reverse().toString());
+
+        if (num1 > num2) {
+            bw.write(Integer.toString(num1));
+        } else {
+            bw.write(Integer.toString(num2));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
