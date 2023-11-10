364c230e9808c383aa112782d62418b028dba2bf
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-11 00:23:43 +0900

    백준 25083 - 문자열 출력

---
+++ baekjoon/advanced1/Baekjoon25083.java
@@ -0,0 +1,19 @@
+package baekjoon.advanced1;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon25083 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("         ,r\'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("    `~\\/\n");
+        bw.write("       |\n");
+        bw.write("       |\n");
+        bw.flush();
+        bw.close();
+    }
+}
