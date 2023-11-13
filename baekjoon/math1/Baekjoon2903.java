36cd9e4d361508cbd93f824ea8a640c680ab0a20
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-13 20:16:19 +0900

    백준 2903 - (1+2^n)^2 계산

---
+++ baekjoon/math1/Baekjoon2903.java
@@ -0,0 +1,22 @@
+package baekjoon.math1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon2903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString((int) Math.pow((1 + Math.pow(2, n)), 2)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
