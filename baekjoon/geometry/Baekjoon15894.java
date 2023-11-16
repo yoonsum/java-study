b76a646163d6fcaedeb5e73505cd12026da9aa37
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 20:52:02 +0900

    백준 15894 - input 값 범위 : long

---
+++ baekjoon/geometry/Baekjoon15894.java
@@ -0,0 +1,20 @@
+package baekjoon.geometry;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Integer.parseInt(br.readLine());
+        bw.write(Long.toString(n * 4));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
