62a88f3e3ff8144c670b53371f3c3f3f6c982967
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 12:55:33 +0900

    백준 25083 수정

--- baekjoon/advanced1/Baekjoon25083.java
+++ baekjoon/advanced1/Baekjoon25083.java
@@ -10,9 +10,9 @@ public class Baekjoon25083 {
         bw.write("         ,r\'\"7\n");
         bw.write("r`-_   ,'  ,/\n");
         bw.write(" \\. \". L_r'\n");
-        bw.write("    `~\\/\n");
-        bw.write("       |\n");
-        bw.write("       |\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |\n");
         bw.flush();
         bw.close();
     }
