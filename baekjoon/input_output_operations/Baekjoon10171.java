ce8d692e00e3f391dcc95d1984619a48a36793d4
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 12:19:49 +0900

    백준 10171 수정

--- baekjoon/input_output_operations/Baekjoon10171.java
+++ baekjoon/input_output_operations/Baekjoon10171.java
@@ -7,10 +7,10 @@ import java.io.OutputStreamWriter;
 public class Baekjoon10171 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("\\    / \\\n");
-        bw.write(" )  (  ')\n");
-        bw.write("(  /   )\n");
-        bw.write("\\ (__) |\n");
+        bw.write("\\    /\\\n");
+        bw.write(" )  ( ')\n");
+        bw.write("(  /  )\n");
+        bw.write(" \\(__)|\n");
 
         bw.flush();
         bw.close();
