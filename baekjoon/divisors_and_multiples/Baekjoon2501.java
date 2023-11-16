d3cedfa76a5175e46dbaf66813450fed3e7efa2f
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 13:58:55 +0900

    주석 변경

--- baekjoon/divisors_and_multiples/Baekjoon2501.java
+++ baekjoon/divisors_and_multiples/Baekjoon2501.java
@@ -20,24 +20,24 @@ public class Baekjoon2501 {
 
         ArrayList<Integer> factor = new ArrayList<Integer>();
 
-        // for (int i = 1; i * i <= n; i++) {
-        // if (i * i == n)
-        // factor.add(i);
-        // else if (n % (i) == 0) {
-        // factor.add(i);
-        // factor.add(n / i);
-        // }
-        // }
-
-        double i_sqrt = Math.sqrt(n);
-        for (int i = 1; i < i_sqrt; i++) {
-            if (i == n) {
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n)
                 factor.add(i);
-            } else if (n % i == 0) {
+            else if (n % (i) == 0) {
                 factor.add(i);
                 factor.add(n / i);
             }
         }
+
+        // double i_sqrt = Math.sqrt(n);
+        // for (int i = 1; i < i_sqrt; i++) {
+        // if (i == n) {
+        // factor.add(i);
+        // } else if (n % i == 0) {
+        // factor.add(i);
+        // factor.add(n / i);
+        // }
+        // }
         Collections.sort(factor);
 
         if (factor.size() >= k) {
