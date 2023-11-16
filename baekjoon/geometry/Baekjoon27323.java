46c6d073565e2b3f9bf666d723f15e4e3789e4be
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 14:45:02 +0900

    백준 27323 - 사각형 넓이 = 가로 * 세로

---
+++ baekjoon/geometry/Baekjoon27323.java
@@ -0,0 +1,22 @@
+package baekjoon.geometry;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString(a * b));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
