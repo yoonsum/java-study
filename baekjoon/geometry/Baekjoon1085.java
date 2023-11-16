8a8c8e15526939c3a48b9d5d608a825f50231dbd
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-16 15:11:49 +0900

    백준 1085 - (x,y)에서 사각형의 각 변까지의 거리중 가장 짧은 거리 찾기

---
+++ baekjoon/geometry/Baekjoon1085.java
@@ -0,0 +1,29 @@
+package baekjoon.geometry;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Baekjoon1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int[] arr = { x, w - x, y, h - y };
+        Arrays.sort(arr);
+        bw.write(Integer.toString(arr[0]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
