8a51f95d854b2434230f091a6af5faa876c1843b
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-17 15:08:57 +0900

    백준 24263 - for문이 n번 수행하기 때문에 O(n) - 최고차항의 차수는 1

---
+++ baekjoon/time_complexity/Baekjoon24263.java
@@ -0,0 +1,23 @@
+package baekjoon.time_complexity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Baekjoon24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        bw.write(Long.toString(n) + "\n");
+        bw.write(Integer.toString(1));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
