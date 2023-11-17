b9c03aa5666b3a8a77289707e0b706375eddfdd8
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-17 14:58:13 +0900

    백준 24262 - 배열 값 중 1개만 리턴하기 때문에 O(1) - 상수 시간이 소요되었기 때문에 최고차항의 차수는 0

---
+++ baekjoon/time_complexity/Baekjoon24262.java
@@ -0,0 +1,8 @@
+package baekjoon.time_complexity;
+
+public class Baekjoon24262 {
+    public static void main(String[] args){
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
