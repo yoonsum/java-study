0b58ecec6af07dc09829bd12754924b06ec004f0
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-21 00:04:43 +0900

    백준 25305 - n개 만큼 입력받아서 내림차순으로 정렬한 후 k번쨰에 있는 값 출력

---
+++ baekjoon/sort/Baekjoon25305.java
@@ -0,0 +1,37 @@
+package baekjoon.sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] < a[j]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+
+        bw.write(Integer.toString(a[k - 1]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
