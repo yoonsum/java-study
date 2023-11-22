2b001236bca4f35725144262ab8dd0a508c7f001
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-11-23 00:18:23 +0900

    백준 10814 - Comparator compare Override 해서 정렬 조건 수정 - 문자열로 입력받은 숫자를 int로 변환한 후 오름차순으로 정렬

---
+++ baekjoon/sort/Baekjoon10814.java
@@ -0,0 +1,31 @@
+package baekjoon.sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, (a1, a2) -> {
+            return Integer.parseInt(a1[0]) - Integer.parseInt(a2[0]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
