453643133e631100c2a2b66bf19333d6f0c710d8
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-12-08 21:31:20 +0900

    main에서 service 생성 - InMemoryTodoRepository를 생성해서 TodoService에 넣어준다(DI)

--- base_java/miniproject_todolist/Main.java
+++ base_java/miniproject_todolist/Main.java
@@ -1,10 +1,13 @@
 package base_java.miniproject_todolist;
 
 import base_java.miniproject_todolist.presentation.Menu;
+import base_java.miniproject_todolist.repository.InMemoryTodoRepository;
+import base_java.miniproject_todolist.service.TodoService;
 
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
+        TodoService todoService = new TodoService(new InMemoryTodoRepository());
+        Menu menu = new Menu(todoService);
 
         while (true) {
             menu.displayMenu();
