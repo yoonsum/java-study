7f52c46322d10d3638a0ef5f33a95615c7369818
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-12-08 22:03:35 +0900

    todoItem 추가, 삭제, 수정. 전체 리스트 함수 작성

--- base_java/miniproject_todolist/service/TodoService.java
+++ base_java/miniproject_todolist/service/TodoService.java
@@ -1,5 +1,7 @@
 package base_java.miniproject_todolist.service;
 
+import java.util.*;
+import base_java.miniproject_todolist.domain.TodoItem;
 import base_java.miniproject_todolist.repository.TodoRepository;
 
 public class TodoService {
@@ -7,5 +9,21 @@ public class TodoService {
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
+    }
+
+    public void addTodo(TodoItem item) {
+        todoRepository.addTodo(item);
+    }
+
+    public void deleteTodo(long id) {
+        todoRepository.deleteTodo(id);
+    }
+
+    public void editTodo(TodoItem item) {
+        todoRepository.editTodo(item);
+    }
+
+    public List<TodoItem> getAllTodos() {
+        return todoRepository.getAllTodos();
     }
 }
