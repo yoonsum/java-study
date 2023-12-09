34e6dd09f905c0e79d31934907cb12d46545ffe0
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-12-09 15:11:34 +0900

    백준 2447 - 반복문 -> 재귀로 변경

--- baekjoon/recursion/Baekjoon2447.java
+++ baekjoon/recursion/Baekjoon2447.java
@@ -4,34 +4,45 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Baekjoon2447 {
+
+    static char[][] board;
+    static int n;
+
+    static void recursion(int count) {
+        if (count < 1) {
+            return;
+        }
+
+        for (int start_x = count; start_x < n; start_x += (count * 3)) {
+            for (int start_y = count; start_y < n; start_y += (count * 3)) {
+
+                for (int i = start_x; i < start_x + count; i++) {
+                    for (int j = start_y; j < start_y + count; j++) {
+                        board[i][j] = ' ';
+                    }
+                }
+            }
+        }
+
+        count /= 3;
+        recursion(count);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        char[][] board = new char[n][n];
+        board = new char[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '*');
         }
 
         int count = n / 3;
 
-        while (count >= 1) {
-            for (int start_x = count; start_x < n; start_x += (count * 3)) {
-                for (int start_y = count; start_y < n; start_y += (count * 3)) {
-
-                    for (int i = start_x; i < start_x + count; i++) {
-                        for (int j = start_y; j < start_y + count; j++) {
-                            board[i][j] = ' ';
-                        }
-                    }
-                }
-            }
-
-            count /= 3;
-        }
+        recursion(count);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
