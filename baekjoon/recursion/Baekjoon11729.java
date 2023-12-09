478ed31a378400c610e17ea7156391a5e8957285
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-12-09 17:03:27 +0900

    백준 11729 - 1 -> 2 로 이동하는 함수를 재귀로 호출해서 이동할 원판이 1개가 되면 출력한 뒤 return - 1 -> 2 재귀가 닫히면 1 -> 3 출력 - 2 -> 3 이동하는 함수 재귀

---
+++ baekjoon/recursion/Baekjoon11729.java
@@ -0,0 +1,38 @@
+package baekjoon.recursion;
+
+import java.io.*;
+
+public class Baekjoon11729 {
+    static int count = 0;
+    static StringBuilder sb = new StringBuilder();
+
+    static void recursion(int num, int from, int tmp, int to) {
+        count++;
+        if (num == 1) {
+            sb.append(from).append(" ").append(to).append("\n");
+            return;
+        }
+
+        // 1 -> 2
+        recursion(num - 1, from, to, tmp);
+        // 1 -> 3
+        sb.append(from).append(" ").append(to).append("\n");
+        // 2 -> 3
+        recursion(num - 1, tmp, to, from);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        recursion(n, 1, 2, 3);
+
+        bw.write(Integer.toString(count) + "\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
