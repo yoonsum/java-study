3256abdecd22e19e31fd68e0c75d0447078160ad
yoonsum <77226422+yoonsum@users.noreply.github.com>
2023-12-09 13:39:12 +0900

    할일 추가에 대한 데이터 입력 유효성 검증 코드 작성 - 날짜 형식에 대한 정규표현식으로 유효성 검증 - 날짜 입력에 대한 데이터 가 Y or N 인지 유효성 검증

--- base_java/miniproject_todolist/presentation/AddTodoHandler.java
+++ base_java/miniproject_todolist/presentation/AddTodoHandler.java
@@ -15,6 +15,17 @@ public class AddTodoHandler {
         this.sc = sc;
     }
 
+    private boolean checkStartEndDate(String start, String end) {
+        return start.matches("^\\d{4}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])$");
+    }
+
+    private boolean checkDateYN(String ans) {
+        if (ans.charAt(0) == 'Y' || ans.charAt(0) == 'N') {
+            return true;
+        }
+        return false;
+    }
+
     public void handle() {
         System.out.println("===== 새로운 할 일을 추가합니다. =====");
 
@@ -24,8 +35,17 @@ public class AddTodoHandler {
         System.out.print("설명 : ");
         String description = sc.nextLine();
 
-        System.out.println("날짜를 설정할까요?(Y or N) : ");
-        String isDate = sc.nextLine();
+        System.out.print("날짜를 설정할까요?(Y or N) : ");
+        String isDate;
+        while (true) {
+            isDate = sc.nextLine();
+
+            if (!checkDateYN(isDate)) {
+                System.out.print("Y or N 으로 입력해주세요. : ");
+            } else {
+                break;
+            }
+        }
 
         TodoItem todo = new TodoItem();
         todo.setTitle(title);
@@ -37,12 +57,16 @@ public class AddTodoHandler {
             System.out.print("종료 날짜(20230101): ");
             String endDate = sc.nextLine();
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
-            try {
-                todo.setStartDate(dateFormat.parse(startDate));
-                todo.setEndDate(dateFormat.parse(endDate));
-            } catch (Exception e) {
-                System.out.println("시작날짜, 종료날짜를 저장할 수 없습니다.");
+            if (checkStartEndDate(startDate, endDate)) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmdd");
+                try {
+                    todo.setStartDate(dateFormat.parse(startDate));
+                    todo.setEndDate(dateFormat.parse(endDate));
+                } catch (Exception e) {
+                    System.out.println("시작날짜, 종료날짜를 저장할 수 없습니다.");
+                }
+            } else {
+                System.out.println("날짜 형식이 맞지 않습니다.");
             }
         } else {
             todo.setStartDate(null);
